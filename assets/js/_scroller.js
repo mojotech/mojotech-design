bbc972ab66ea982768c3e763ae99a13aba1c8cb0
Jesse Hoyos <jesse@mojotech.com>
2018-08-31 00:16:24 -0400

    [Chore] Prettier

--- assets/js/_scroller.js
+++ assets/js/_scroller.js
@@ -1,31 +1,31 @@
-import 'intersection-observer'
-import scrollama from 'scrollama'
-import { debounce } from './_helpers'
+import "intersection-observer";
+import scrollama from "scrollama";
+import { debounce } from "./_helpers";
 
 export function scrollController() {
-  const scroller = scrollama()
+  const scroller = scrollama();
   const scrollConfig = {
-    step: '.step',
+    step: ".step",
     threshhold: 2,
-    debug: true,
-    offset: 0.8,
-  }
+    // debug: true,
+    offset: 0.8
+  };
 
   const handleEnter = ({ element, direction }) =>
-    (document.body.dataset.theme = element.dataset.themeset)
+    (document.body.dataset.theme = element.dataset.themeset);
 
   const handleExit = ({ element, direction }) => {
-    console.log(`exiting: ${element} direction: ${direction}`)
-  }
+    console.log(`exiting: ${element} direction: ${direction}`);
+  };
 
-  const _handleEnter = debounce(handleEnter, 200)
+  const _handleEnter = debounce(handleEnter, 200);
 
-  window.addEventListener('resize', () => {
-    scroller.resize()
-  })
+  window.addEventListener("resize", () => {
+    scroller.resize();
+  });
 
   scroller
     .setup(scrollConfig)
     .onStepEnter(_handleEnter)
-    .onStepExit(handleExit)
+    .onStepExit(handleExit);
 }
