0776f67d0d96cb0387a10cbc82fa7ef4c13cdbab
Jesse Hoyos <jesse@mojotech.com>
2018-09-07 15:55:49 -0400

    animation stagger classes

--- assets/js/_helpers.js
+++ assets/js/_helpers.js
@@ -10,3 +10,15 @@ export const debounce = (callback, delay) => {
     timeout = setTimeout(call, delay);
   };
 };
+
+export const lerp = (min, max, t) => min * (1 - t) + max * t;
+
+export const onReady = fn => {
+  if (document.readyState !== "loading") fn();
+  else document.addEventListener("DOMContentLoaded", fn);
+};
+
+export const queryArray = (selector, node) => {
+  if (!node) node = document.body;
+  return Array.prototype.slice.call(node.querySelectorAll(selector));
+};
