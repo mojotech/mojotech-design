e3e26bb8830d6b6929157752ec21c80ecd64e224
chriswynn <chris@wynncodes.com>
2020-01-14 12:12:50 -0500

    feat: add hero to homepage

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,28 @@
 import * as React from "react";
+import { Text, Wrap } from "@mojotech/mojo-ui";
+
 import Layout from "../components/Layout";
+import RatioBox from "../components/RatioBox";
 
 const Index: React.FunctionComponent = () => (
   <Layout>
-    <h1>hello</h1>
+    <Wrap pt={6}>
+      <RatioBox aspectRatio={[21, 9]}>
+        <Text mb={0} fontSize={5} fontFamily="display" as="h1">
+          We're Mojotech's
+          <br />
+          design team.
+        </Text>
+      </RatioBox>
+    </Wrap>
+    <Wrap mb={5}>
+      <Text fontSize={3} as="h2">
+        Since 2008, we’ve been building serious products for clients like
+        UnderArmour, Shell, and Credit Karma. Our work is driven by what our
+        clients need, not our egos—we’re here to build the right thing, not to
+        be right.
+      </Text>
+    </Wrap>
   </Layout>
 );
 
