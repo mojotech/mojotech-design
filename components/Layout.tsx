80ce8691ee5967ee9d0c916954c95543fae3b5dd
chriswynn <chris@wynncodes.com>
2020-01-14 11:51:11 -0500

    feat: add footer component

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,11 +1,14 @@
 import * as React from "react";
-import Header from "./Header";
 import { SchemeProvider } from "@mojotech/mojo-ui";
+
+import Header from "./Header";
+import Footer from "./Footer";
 
 const Layout: React.FC = props => (
   <SchemeProvider scheme="dark">
     <Header />
     {props.children}
+    <Footer />
   </SchemeProvider>
 );
 
