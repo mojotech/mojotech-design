19b2731d41d34c087ca21240121fa6f010fd83a1
chriswynn <chris@wynncodes.com>
2020-01-21 08:15:45 -0500

    refactor: turn _app page into function

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,3 @@
-import App from "next/app";
 import Head from "next/head";
 import * as React from "react";
 import { AppProps } from "next/app";
@@ -6,36 +5,32 @@ import { ThemeProvider } from "emotion-theming";
 import { Global } from "@emotion/core";
 import { ThemeDecorator, BaseTheme } from "@mojotech/mojo-ui";
 
-export default class extends App {
-  render() {
-    const { Component, pageProps }: AppProps = this.props;
-    const { colors } = BaseTheme;
+const App = ({ Component, pageProps }: AppProps) => {
+  const { colors } = BaseTheme;
 
-    return (
-      <ThemeProvider theme={BaseTheme}>
-        <Head>
-          <title>MojoTech Design</title>
-          <meta
-            name="viewport"
-            content="initial-scale=1.0, width=device-width"
-          />
-        </Head>
-        <Global
-          styles={{
-            html: {
-              backgroundColor: colors.dark,
-            },
-            "::selection": {
-              backgroundColor: colors.mojogreen,
-              color: "white",
-              textShadow: `0 1px 0 ${colors.meangreen}80`,
-            },
-          }}
-        />
-        <ThemeDecorator>
-          <Component {...pageProps} />
-        </ThemeDecorator>
-      </ThemeProvider>
-    );
-  }
-}
+  return (
+    <ThemeProvider theme={BaseTheme}>
+      <Head>
+        <title>MojoTech Design</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
+      <Global
+        styles={{
+          html: {
+            backgroundColor: colors.gray,
+          },
+          "::selection": {
+            backgroundColor: colors.mojogreen,
+            color: "white",
+            textShadow: `0 1px 0 ${colors.meangreen}80`,
+          },
+        }}
+      />
+      <ThemeDecorator>
+        <Component {...pageProps} />
+      </ThemeDecorator>
+    </ThemeProvider>
+  );
+};
+
+export default App;
