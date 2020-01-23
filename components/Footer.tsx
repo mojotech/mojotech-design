07d337a41cbc3a28a3d2315cb5312a65a95d9cd6
chriswynn <chris@wynncodes.com>
2020-01-23 13:20:20 -0500

    refactor: update Footer to fit design

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,43 +1,83 @@
 /** @jsx jsx */
 import * as React from "react";
+import Link from "next/link";
+import { motion, useViewportScroll } from "framer-motion";
 import { jsx } from "@emotion/core";
-import { Box, Flex, TextLink } from "@mojotech/mojo-ui";
-import Logo from "./Logo";
+import { Box, Wrap, AutoGrid, Text } from "@mojotech/mojo-ui";
 
-const Footer: React.FC = () => (
-  <Box background="#1A1723" as="footer">
-    <Flex
-      flexDirection={["column", "row"]}
-      m="0 auto"
-      py={4}
-      px={3}
-      maxw={[0, 3]}
-      w="100%"
-    >
-      <Logo />
-      <TextLink
-        href="https://dribbble.com/mojotech/"
-        rel="noopener noreferrer"
-        target="_blank"
-        scheme="dark"
-        fontSize={3}
-        mx={[0, 4]}
-        underline
-      >
-        Check out our Dribbble
-      </TextLink>
-      <TextLink
-        href="https://mojotech.com/"
-        rel="noopener noreferrer"
-        target="_blank"
-        scheme="dark"
-        fontSize={3}
-        underline
-      >
-        Learn about MojoTech
-      </TextLink>
-    </Flex>
-  </Box>
-);
+import Logo from "../components/Logo";
+import { mq } from "../lib/utils";
+
+const logoVariants = {
+  bottom: {
+    y: 0,
+    opacity: 1,
+    transition: {
+      y: { stiffness: 1000, velocity: -1000 },
+    },
+  },
+  top: {
+    y: 75,
+    opacity: 0,
+    transition: {
+      y: { stiffness: 1000 },
+    },
+  },
+};
+
+const Footer: React.FunctionComponent = () => {
+  const [bottom, setBottom] = React.useState(false);
+  const { scrollYProgress } = useViewportScroll();
+
+  React.useEffect(() => {
+    scrollYProgress.onChange(y => {
+      y > 0.95 ? setBottom(true) : setBottom(false);
+    });
+  });
+
+  return (
+    <Box as="footer" paddingY={5} position="relative">
+      <Wrap position="relative" maxWidth={3}>
+        <motion.div
+          animate={bottom ? "bottom" : "top"}
+          variants={logoVariants}
+          css={mq({
+            display: ["none", "block"],
+            position: "absolute",
+          })}
+        >
+          <Logo />
+        </motion.div>
+      </Wrap>
+      <Wrap>
+        <AutoGrid
+          gridTemplateColumns={["repeat(1, 1fr)", "repeat(2, 1fr)"]}
+          gridGap={[3, 4]}
+          css={{
+            lineHeight: 2.2,
+          }}
+        >
+          <Box as="ul">
+            <li>
+              <Link scroll={false} href="/who-we-are/" passHref>
+                <Text as="a">Internships</Text>
+              </Link>
+            </li>
+            <li>
+              <Link scroll={false} href="/how-we-work/" passHref>
+                <Text as="a">Learn About MojoTech</Text>
+              </Link>
+            </li>
+            <li>
+              <Link scroll={false} href="/services/" passHref>
+                <Text as="a">Check out or Dribbble</Text>
+              </Link>
+            </li>
+          </Box>
+        </AutoGrid>
+      </Wrap>
+    </Box>
+  );
+};
 
 export default Footer;
