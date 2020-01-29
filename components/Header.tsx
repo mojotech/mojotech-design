7e89bb308cd0eb6362fba23d2f43e85ef38d28ac
chriswynn <chris@wynncodes.com>
2020-01-29 10:39:53 -0500

    refactor: adjust header interaction to match design

--- components/Header.tsx
+++ components/Header.tsx
@@ -2,28 +2,59 @@
 import * as React from "react";
 import { jsx } from "@emotion/core";
 import Link from "next/link";
-import { Text, Flex } from "@mojotech/mojo-ui";
+import { Flex, Button } from "@mojotech/mojo-ui";
+import { motion } from "framer-motion";
+import { useScrollPos } from "../lib/use-scroll-pos";
 
 import Logo from "./Logo";
 
-const Header: React.FC = () => (
-  <Flex
-    pos="absolute"
-    jfc="space-between"
-    top={[16, "8vh"]}
-    px={3}
-    left="50%"
-    transform="translateX(-50%)"
-    maxw={[0, 3]}
-    w="100%"
-  >
-    <Logo />
-    <Link href="/">
-      <Flex ali="center" jfc="center" as="a" px={4} py={2} bgc="mojogreen">
-        <Text as="span">Apply</Text>
-      </Flex>
-    </Link>
-  </Flex>
-);
+const headerVariants = {
+  bottom: {
+    translateY: -50,
+    opacity: 0,
+    transition: {
+      translateY: { stiffness: 1000, velocity: -100 },
+    },
+  },
+  top: {
+    translateY: 0,
+    opacity: 1,
+    transition: {
+      translateY: { stiffness: 1000 },
+    },
+  },
+};
+
+const Header: React.FC = () => {
+  const scrollPos = useScrollPos();
+  return (
+    <Flex
+      pos="fixed"
+      jfc="space-between"
+      top={[16, "8vh"]}
+      px={3}
+      left="50%"
+      transform="translateX(-50%)"
+      maxw={[0, 3]}
+      w="100%"
+      zIndex={1}
+    >
+      <motion.div
+        animate={scrollPos === "bottom" ? "bottom" : "top"}
+        variants={headerVariants}
+      >
+        <Logo />
+      </motion.div>
+      <motion.div
+        animate={scrollPos === "bottom" ? "bottom" : "top"}
+        variants={headerVariants}
+      >
+        <Link href="/">
+          <Button paddingX={4}>Apply</Button>
+        </Link>
+      </motion.div>
+    </Flex>
+  );
+};
 
 export default Header;
