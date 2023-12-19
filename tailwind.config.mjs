f96eb1c4d7a6fddd3a7bae735918a7f5e44b3b75
Chris Wynn <cwynn@mojotech.com>
2023-12-19 09:48:53 -0500

    chore: add colors to tailwind config

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,25 @@
 export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        bgDark: "#14111D",
+        textLight: "#FFFFFF",
+        mkOrange: "#FA4D31",
+        angiBlue: "#003359",
+        ckPurple: "#BD65DB",
+        dowYellow: "#FFED4D",
+        mlBlue: "#CFD9ED",
+        betterUpPink: "#F7DDE1",
+        amicaTeal: "#00A891",
+        novaGreen: "#C3EA8B",
+        uaGray: "#454545",
+        springBlue: "#6A79F9",
+        ampBlue: "#325E87",
+        bcbsBlue: "#4BB2E5",
+        uwBlue: "#125790",
+      },
+    },
   },
   plugins: [],
 };
