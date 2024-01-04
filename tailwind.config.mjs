b3287aece298f40ce4b76ee8decf8f20fd405fe4
Jesse Hoyos <jesse@mojotech.com>
2024-01-04 16:12:42 -0500

    wip: move all one-off colors out of theme and into arbitrary classes

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,26 +1,12 @@
-/** @type {import('tailwindcss').Config} */
 const defaultTheme = require("tailwindcss/defaultTheme");
 
+/** @type {import('tailwindcss').Config} */
 export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
     extend: {
       colors: {
-        bgDark: "#14111D",
-        textLight: "#FFFFFF",
-        mkOrange: "#FA4D31",
-        angiBlue: "#003359",
-        ckPurple: "#BD65DB",
-        dowYellow: "#FFED4D",
-        mlBlue: "#CFD9ED",
-        betterUpPink: "#F7DDE1",
-        amicaTeal: "#00A891",
-        novaGreen: "#C3EA8B",
-        uaGray: "#454545",
-        springBlue: "#6A79F9",
-        ampBlue: "#325E87",
-        bcbsBlue: "#4BB2E5",
-        uwBlue: "#125790",
+        dark: "#14111D",
       },
     },
     fontFamily: {
