ebd05c7f142196a1163a65b8b559aa7f6177c00c
Chris Wynn <cwynn@mojotech.com>
2024-01-10 11:22:26 -0500

    chore: animation tweaks

--- src/lib/workAnimation.ts
+++ src/lib/workAnimation.ts
@@ -1,17 +1,30 @@
-import { animate } from "motion";
+import { animate, inView } from "motion";
 
-export default (container: string, image: string) => {
+export default (container: string, image: string, logo: string) => {
   const containerNode = document.querySelector(container)!;
   const imageNode = containerNode.querySelector(image)!;
+  const logoNode = containerNode.querySelector(logo)!;
 
   containerNode.addEventListener("mouseenter", () => {
+    animate(
+      logoNode,
+      {
+        opacity: 0,
+        y: 10,
+      },
+      {
+        duration: 0.2,
+        easing: "ease-in-out",
+      },
+    );
     animate(
       imageNode,
       {
         opacity: 1,
       },
       {
-        duration: 0.4,
+        delay: 0.1,
+        duration: 0.3,
         easing: "ease-in-out",
       },
     );
@@ -19,12 +32,24 @@ export default (container: string, image: string) => {
 
   containerNode.addEventListener("mouseleave", () => {
     animate(
+      logoNode,
+      {
+        opacity: 1,
+        y: 0,
+      },
+      {
+        duration: 0.3,
+        delay: 0.1,
+        easing: "ease-in-out",
+      },
+    );
+    animate(
       imageNode,
       {
         opacity: 0,
       },
       {
-        duration: 0.4,
+        duration: 0.2,
         easing: "ease-in-out",
       },
     );
